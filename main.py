9cfeaef3238f4bf8f747c30ba4f3158cb0a1f131
Pranav Singh <singh.pranav10@gmail.com>
2025-03-08 22:45:33 -0500

    Prepare Flask API for Render

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pandas.core.dtypes.missing import isna
 
 app = Flask(__name__)
 
+CSV_DIR = os.path.abspath(os.path.join(os.path.dirname('./'), "csvs"))
 
 @app.route('/')
 def index():
@@ -14,16 +15,15 @@ def index():
 
 @app.route('/agg/address')
 def agg_address():
-  csv_dir = "./csvs/agg/"
-  csv_file = "agg_address.csv"
-  csv_path = os.path.join(csv_dir, csv_file)
+  csv_file = "agg/agg_address.csv"
+  csv_path = os.path.join(CSV_DIR, csv_file)
 
   # Also make sure the requested csv file does exist
   if not os.path.isfile(csv_path):
     return "ERROR: file %s was not found on the server" % csv_file
   # Send the file back to the client
   csv_data = pd.read_csv(csv_path)
-  headers = {'Content-type': 'text/plain'}
+  headers = {'Content-type': 'text/csv'}
   response = Response(csv_data.to_csv(index=False), headers=headers)
   # response.headers["Content-Disposition"] = "attachment; filename=users.csv"
 
@@ -32,16 +32,15 @@ def agg_address():
 
 @app.route('/agg/code')
 def agg_code():
-  csv_dir = "./csvs/agg/"
-  csv_file = "agg_code.csv"
-  csv_path = os.path.join(csv_dir, csv_file)
+  csv_file = "agg/agg_code.csv"
+  csv_path = os.path.join(CSV_DIR, csv_file)
 
   # Also make sure the requested csv file does exist
   if not os.path.isfile(csv_path):
     return "ERROR: file %s was not found on the server" % csv_file
   # Send the file back to the client
   csv_data = pd.read_csv(csv_path)
-  headers = {'Content-type': 'text/plain'}
+  headers = {'Content-type': 'text/csv'}
   response = Response(csv_data.to_csv(index=False), headers=headers)
 
   return response
@@ -49,18 +48,16 @@ def agg_code():
 
 @app.route('/agg/code/<id>')
 def agg_code_by_id(id):
-  csv_dir = "./csvs/agg/"
-  csv_file = "agg_code.csv"
-  csv_path = os.path.join(csv_dir, csv_file)
+  csv_file = "agg/agg_code.csv"
+  csv_path = os.path.join(CSV_DIR, csv_file)
 
   # Also make sure the requested csv file does exist
   if not os.path.isfile(csv_path):
     return "ERROR: file %s was not found on the server" % csv_file
   # Send the file back to the client
   csv_data = pd.read_csv(csv_path)
-  headers = {'Content-type': 'text/plain'}
-  print(id)
-  csv_data = csv_data[csv_data['code'] == int(id)]
+  headers = {'Content-type': 'text/csv'}
+  csv_data = csv_data[csv_data['Code'] == int(id)]
   response = Response(csv_data.to_csv(index=False), headers=headers)
 
   return response
@@ -68,16 +65,15 @@ def agg_code_by_id(id):
 
 @app.route('/agg/prov')
 def agg_prov():
-  csv_dir = "./csvs/agg/"
-  csv_file = "agg_prov.csv"
-  csv_path = os.path.join(csv_dir, csv_file)
+  csv_file = "agg/agg_prov.csv"
+  csv_path = os.path.join(CSV_DIR, csv_file)
 
   # Also make sure the requested csv file does exist
   if not os.path.isfile(csv_path):
     return "ERROR: file %s was not found on the server" % csv_file
   # Send the file back to the client
   csv_data = pd.read_csv(csv_path)
-  headers = {'Content-type': 'text/plain'}
+  headers = {'Content-type': 'text/csv'}
   response = Response(csv_data.to_csv(index=False), headers=headers)
 
   return response
@@ -85,16 +81,15 @@ def agg_prov():
 
 @app.route('/agg/fine_per_day')
 def agg_fine_per_day():
-  csv_dir = "./csvs/agg/"
-  csv_file = "agg_fine_per_day.csv"
-  csv_path = os.path.join(csv_dir, csv_file)
+  csv_file = "agg/agg_fine_per_day.csv"
+  csv_path = os.path.join(CSV_DIR, csv_file)
 
   # Also make sure the requested csv file does exist
   if not os.path.isfile(csv_path):
     return "ERROR: file %s was not found on the server" % csv_file
   # Send the file back to the client
   csv_data = pd.read_csv(csv_path)
-  headers = {'Content-type': 'text/plain'}
+  headers = {'Content-type': 'text/csv'}
   response = Response(csv_data.to_csv(index=False), headers=headers)
 
   return response
@@ -102,16 +97,15 @@ def agg_fine_per_day():
 
 @app.route('/agg/dayofweek')
 def agg_dayofweek():
-  csv_dir = "./csvs/agg/"
-  csv_file = "agg_dayofweek.csv"
-  csv_path = os.path.join(csv_dir, csv_file)
+  csv_file = "agg/agg_dayofweek.csv"
+  csv_path = os.path.join(CSV_DIR, csv_file)
 
   # Also make sure the requested csv file does exist
   if not os.path.isfile(csv_path):
     return "ERROR: file %s was not found on the server" % csv_file
   # Send the file back to the client
   csv_data = pd.read_csv(csv_path)
-  headers = {'Content-type': 'text/plain'}
+  headers = {'Content-type': 'text/csv'}
   response = Response(csv_data.to_csv(index=False), headers=headers)
 
   return response
@@ -119,13 +113,12 @@ def agg_dayofweek():
 
 @app.route('/dayofweek/<day>')
 def dayofweek(day):
-  csv_dir = "./csvs/dayofweek/" + day
-  csv_file = "*.csv"
+  csv_file = "dayofweek/" + day + "/*.csv"
   csv_paths = []
-  if not os.listdir(csv_dir):
+  if not os.listdir(CSV_DIR + "/dayofweek/" + day):
     return "ERROR: path %s was empty" % day
-  for f in os.listdir(csv_dir):
-    csv_paths.append(os.path.join(csv_dir, f))
+  for f in os.listdir(CSV_DIR + "/dayofweek/" + day):
+    csv_paths.append(os.path.join(CSV_DIR + "/dayofweek/" + day, f))
 
   # Also make sure the requested csv file does exist
   for csv_path in csv_paths:
@@ -136,7 +129,7 @@ def dayofweek(day):
 
   csv_data = pd.concat([pd.read_csv(f) for f in csv_paths], ignore_index=True)
   csv_data.name = day
-  headers = {'Content-type': 'text/plain'}
+  headers = {'Content-type': 'text/csv'}
   response = Response(
       csv_data.sort_values(by="issue_date").to_csv(index=False),
       headers=headers)
@@ -146,13 +139,12 @@ def dayofweek(day):
 
 @app.route('/total')
 def total():
-  csv_dir = "./csvs/total/"
-  csv_file = "*.csv"
+  csv_file = "total/*.csv"
   csv_paths = []
-  if not os.listdir(csv_dir):
+  if not os.listdir(CSV_DIR):
     return "ERROR: path %s was empty"
-  for f in os.listdir(csv_dir):
-    csv_paths.append(os.path.join(csv_dir, f))
+  for f in os.listdir(CSV_DIR):
+    csv_paths.append(os.path.join(CSV_DIR, f))
 
   # Also make sure the requested csv file does exist
   for csv_path in csv_paths:
@@ -162,7 +154,7 @@ def total():
   # Send the file back to the client
 
   csv_data = pd.concat([pd.read_csv(f) for f in csv_paths], ignore_index=True)
-  headers = {'Content-type': 'text/plain'}
+  headers = {'Content-type': 'text/csv'}
   response = Response(
       csv_data.sort_values(by=["Issue_Date", 'Issue_Time']).to_csv(index=False),
       headers=headers)
